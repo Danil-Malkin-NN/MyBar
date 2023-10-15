88199c57ac4ee458a776a4478d9fb649eed7fa4b
Danil <danilmalkin2015@gmail.com>
2023-10-15 18:24:31 +0300

    Initial commit

---
+++ src/main/java/ru/nino/mybar/MyBarApplication.java
@@ -0,0 +1,10 @@
+package ru.nino.mybar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBarApplication.class, args
---
+++ src/test/java/ru/nino/mybar/MyBarApplicationTests.java
@@ -0,0 +1,10 @@
+package ru.nino.mybar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBarApplicationTests {
+
+    @Test
+    void contextLoads() {
