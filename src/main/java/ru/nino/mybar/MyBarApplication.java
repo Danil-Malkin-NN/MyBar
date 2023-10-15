e97f36967ea29781987fa78617d67533c2b15b83
Danil <danilmalkin2015@gmail.com>
2023-10-15 18:26:33 +0300

    инит 2

--- src/main/java/ru/nino/mybar/MyBarApplication.java
+++ src/main/java/ru/nino/mybar/MyBarApplication.java
@@ -7,4 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyBarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyBarApplication.class, args
+        SpringApplication.run(MyBarApplication.class, args);
+    }
+
+}
--- src/test/java/ru/nino/mybar/MyBarApplicationTests.java
+++ src/test/java/ru/nino/mybar/MyBarApplicationTests.java
@@ -8,3 +8,6 @@ class MyBarApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+}
