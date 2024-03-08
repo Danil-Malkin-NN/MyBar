9491c27dbd9107f4964e9e933d04f18f29ff4633
Danil <danilmalkin2015@gmail.com>
2024-03-08 14:35:42 +0300

    Дописал сущности

--- src/main/java/ru/nino/mybar/entity/Ingredient.java
+++ src/main/java/ru/nino/mybar/entity/Ingredient.java
@@ -1,19 +1,21 @@
 package ru.nino.mybar.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
-
 @Entity
+@EqualsAndHashCode(callSuper = true)
 public class Ingredient extends IdEntity {
 
+    @Schema(description = "Название ингредиента", example = "водка")
     private String name;
 
     @Column(length = 3000)
+    @Schema(description = "Описание ингредиента", example = "водка")
     private String description;
 
 }
--- src/main/java/ru/nino/mybar/entity/Instrument.java
+++ src/main/java/ru/nino/mybar/entity/Instrument.java
@@ -1,6 +1,7 @@
 package ru.nino.mybar.entity;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
@@ -9,9 +10,11 @@ import lombok.Data;
 @Data
 public class Instrument extends IdEntity {
 
+    @Schema(description = "Название инструмента", example = "Шейкер")
     private String name;
 
     @Column(length = 1000)
+    @Schema(description = "Описание инструмента", example = "Инструмент для смешивания коктейлей")
     private String description = "";
 
 
