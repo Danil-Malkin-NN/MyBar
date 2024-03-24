b862b3c677e98e68ed08a20afac067cea35fac03
Danil <danilmalkin2015@gmail.com>
2024-03-24 22:26:23 +0300

    Обновил ограничения на классах

--- src/main/java/ru/nino/mybar/entity/Ingredient.java
+++ src/main/java/ru/nino/mybar/entity/Ingredient.java
@@ -8,14 +8,14 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
 public class Ingredient extends IdEntity {
 
-    @Column(name = "name", unique = true)
+    @Column(name = "name", unique = true, columnDefinition = "TEXT")
     @Schema(description = "Название ингредиента", example = "водка")
     private String name;
 
-    @Column(length = 3000)
+    @Column(columnDefinition = "TEXT")
     @Schema(description = "Описание ингредиента", example = "водка")
     private String description;
 
