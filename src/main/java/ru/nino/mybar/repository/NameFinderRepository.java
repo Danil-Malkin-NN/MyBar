d3efc4c8ac1f111f1cdb87def1e5b0daa233a356
Малькин Данила <53613490+Danil-Malkin-NN@users.noreply.github.com>
2025-05-11 12:11:42 +0300

    Добавил поиск по ингредиентам и коктейлям (#22)

--- src/main/java/ru/nino/mybar/repository/NameFinderRepository.java
+++ src/main/java/ru/nino/mybar/repository/NameFinderRepository.java
@@ -2,7 +2,6 @@ package ru.nino.mybar.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import ru.nino.mybar.entity.IdEntity;
 
 import java.util.List;
@@ -10,14 +9,14 @@ import java.util.List;
 public interface NameFinderRepository<ENTITY extends IdEntity> extends DefaultRepository<ENTITY> {
 
     default List<ENTITY> findTop5ByNameLikeIgnoreCase(String name) {
-        return null;
+        throw new UnsupportedOperationException("Метод findTop5ByNameLikeIgnoreCase не переопределён в этом репозитории.");
     }
 
     default ENTITY findByName(String s) {
-        return null;
+        throw new UnsupportedOperationException("Метод findByName не переопределён в этом репозитории.");
     }
 
-
-
-    ;
+    default Page<ENTITY> findByNameContainingIgnoreCase(String name, Pageable pageable) {
+        throw new UnsupportedOperationException("Метод findByNameContainingIgnoreCase не переопределён в этом репозитории.");
+    }
 }
