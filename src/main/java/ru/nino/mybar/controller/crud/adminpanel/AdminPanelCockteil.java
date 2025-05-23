a81e008309b1f13ad0f75fde57d2c6b1e0ee5bbc
Danil <danilmalkin2015@gmail.com>
2025-05-23 21:14:59 +0300

    Исправил баг

--- src/main/java/ru/nino/mybar/controller/crud/adminpanel/AdminPanelCockteil.java
+++ src/main/java/ru/nino/mybar/controller/crud/adminpanel/AdminPanelCockteil.java
@@ -1,10 +1,8 @@
 package ru.nino.mybar.controller.crud.adminpanel;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
