57aa78d83944162840a17cd83a559975335ee6a4
Danil <danilmalkin2015@gmail.com>
2025-06-03 09:35:11 +0300

    Работа через MVC мне не нравится, сложно и неприятно

--- src/main/java/ru/nino/mybar/controller/mwc/admin/CocktailAdmin.java
+++ src/main/java/ru/nino/mybar/controller/mwc/admin/CocktailAdmin.java
@@ -4,23 +4,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.nino.mybar.dto.show.CocktailDto;
+import ru.nino.mybar.service.CocktailServiceImpl;
 import ru.nino.mybar.service.IngredientServiceImpl;
 import ru.nino.mybar.service.InstrumentServiceImpl;
 
 @Controller
+@RequestMapping("admin/cocktails")
 @RequiredArgsConstructor
 public class CocktailAdmin {
 
     private final IngredientServiceImpl ingredientService;
     private final InstrumentServiceImpl instrumentService;
+    private final CocktailServiceImpl cocktailService;
 
-    @GetMapping("/cocktails/new")
+    @GetMapping("/new")
     public String showCreateForm(Model model) {
         model.addAttribute("ingredients", ingredientService.getAll());
         model.addAttribute("instruments", instrumentService.getAll());
         model.addAttribute("cocktailDto", new CocktailDto());
         return "admin/create-cocktail";
     }
+
+    @PostMapping("/create")
+    public String createCocktail(@ModelAttribute CocktailDto cocktailDto) {
+        cocktailService.create(cocktailDto);
+        return "redirect:/cocktails";
+    }
+
 }
 
